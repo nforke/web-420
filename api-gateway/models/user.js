3c27b467200956fba87697238e1138289dc82029
Nicole Forke <n.forke@gmail.com>
2020-05-23 12:57:57 -0500

    Udating files for API Gateway Part III

--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -3,7 +3,7 @@
 ; Title:  user.js
 ; Author: Nicole Forke
 ; Modified By: Nicole Forke
-; Date:   08 May 2020
+; Date:   23 May 2020
 ; Description: User model and schema
 ;============================================================
 */
@@ -24,6 +24,17 @@ var userSchema = new mongoose.Schema({
 
 // export the model
 module.exports = mongoose.model('User', userSchema);
+
+// user.save is used to add a new user in our database
+module.exports.add = (user, callback) => {
+  user.save(callback);
+};
+
+// user.getById used to find a user by id
+module.exports.getById = (id, callback) => {
+  var query = {_id: id};
+  User.findById(query, callback);
+};
 
 /**
  * Database queries
--- api-gateway/routes/index.js
+++ api-gateway/routes/index.js
@@ -1,4 +1,17 @@
+/*
+=============================================
+; Title: Assignment 4.3
+; Author: Nicole Forke
+; Date: 22 May 2020
+; Modified By: Nicole Forke
+; Description: API-Gateway Part III
+=============================================
+*/
+
+// require statements
 var express = require('express');
+
+// routes
 var router = express.Router();
 
 /* GET home page. */
